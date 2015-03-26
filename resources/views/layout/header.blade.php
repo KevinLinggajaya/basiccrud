54dca08e0153d41a5050fceaa382f9321e2ed477
Kevin Linggajaya <kevin.linggajaya@gmail.com>
2015-03-27 03:50:33 +0700

    fix home url

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -7,7 +7,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="/">{{ env('TITLE') }}</a>
+			<a class="navbar-brand" href="{{ action('HomeController@index') }}">{{ env('TITLE') }}</a>
 		</div>
 		<div class="collapse navbar-collapse" id="navbar-collapsed">
 			<ul class="nav navbar-nav navbar-right">
