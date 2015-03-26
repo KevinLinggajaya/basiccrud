cf1e21f815b0f2a26061e6aa1f28ae7c14bded1a
Kevin Linggajaya <kevin.linggajaya@gmail.com>
2015-03-27 03:48:18 +0700

    migration for order and orderDetails

---
+++ database/migrations/2015_03_26_162708_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('orders', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->varchar('order_no', 12);
+			$table->decimal('total_price', 10, 2);
+			$table->text('shipping_address');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('orders');
+	}
+
+}
