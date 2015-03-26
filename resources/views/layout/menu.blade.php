7c87457e6af55d81604a370ecfde46febedf5649
Kevin Linggajaya <kevin.linggajaya@gmail.com>
2015-03-27 03:50:49 +0700

    add new menu for order

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -1,3 +1,4 @@
 <ul class="nav nav-stacked" role="navigation">
-	<li role="presentation" class="{{ strpos(Request::path(),'products') !== false ? 'active' : '' }}"><a href="{{action('ProductController@index')}}"> <span class="glyphicon glyphicon-dashboard"></span> Product</a></li>
+	<li role="presentation" class="{{ strpos(Request::path(),'products') !== false ? 'active' : '' }}"><a href="{{action('ProductController@index')}}"> <span class="glyphicon glyphicon-list"></span> Product</a></li>
+	<li role="presentation" class="{{ strpos(Request::path(),'orders') !== false ? 'active' : '' }}"><a href="{{action('OrderController@index')}}"> <span class="glyphicon glyphicon-tag"></span> Order</a></li>
 </ul>
