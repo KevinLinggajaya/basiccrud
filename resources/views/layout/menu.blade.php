60bf0403153f0f03e8cf4fd80b6fc991a78a3dd2
Kevin Linggajaya <kevin.linggajaya@gmail.com>
2015-04-13 00:47:58 +0700

    display menu for authorized user only

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -1,5 +1,7 @@
+@if (Auth::check())
 <ul class="nav nav-stacked" role="navigation">
 	<li role="presentation" class="{{ strpos(Request::path(),'products') !== false ? 'active' : '' }}"><a href="{{action('ProductController@index')}}"> <span class="glyphicon glyphicon-list"></span> Product</a></li>
 	<li role="presentation" class="{{ strpos(Request::path(),'orders') !== false ? 'active' : '' }}"><a href="{{action('OrderController@index')}}"> <span class="glyphicon glyphicon-tag"></span> Order</a></li>
 	<li role="presentation" class="{{ strpos(Request::path(),'reports') !== false ? 'active' : '' }}"><a href="{{action('ReportController@index')}}"> <span class="glyphicon glyphicon-paste"></span> Report</a></li>
 </ul>
+@endif
